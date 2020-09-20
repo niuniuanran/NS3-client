b2a8b70fc505af344a5e5049acb45334197aa385
niuniuanran <irene.anran.niu@gmail.com>
2020-09-20 22:03:30 +1200

    Install aws amplify and add config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "anran-notes-app"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://av5qe7z5a0.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_ytYxYYcfb",
+        APP_CLIENT_ID: "25cf22m85stp0p7ega0tnladps",
+        IDENTITY_POOL_ID: "us-east-1:5517353c-075b-4f1c-808c-652cebe3a0a6"
+    }
+};
