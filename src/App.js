30f11d26fda88c17206208cd751c351f25a18000
niuniuanran <irene.anran.niu@gmail.com>
2020-09-20 22:29:12 +1200

    Add authenticated state to App

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Navbar, Nav, NavItem } from "react-bootstrap";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import {Navbar, Nav, NavItem} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
 import "./App.css";
-import Routes from "./Routes";
-import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
-  return (
-      <div className="App container">
-        <Navbar fluid collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">NS3</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-            <Navbar.Collapse>
-                <Nav pullRight>
-                    <LinkContainer to={"/signup"}>
-                    <NavItem>Sign Up</NavItem>
-                    </LinkContainer>
-                    <LinkContainer to={"/login"}>
-                        <NavItem>Log In</NavItem>
-                    </LinkContainer>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-          <Routes />
-      </div>
-  );
+    const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+    return (
+        <div className="App container">
+            <Navbar fluid collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to="/">NS3</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle/>
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav pullRight>
+                        <LinkContainer to={"/signup"}>
+                            <NavItem>Sign Up</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to={"/login"}>
+                            <NavItem>Log In</NavItem>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Routes/>
+        </div>
+    );
 }
 
 export default App;
