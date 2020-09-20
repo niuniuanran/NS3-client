e6669b7a0669cd1be42e2603410bbada3e0771f8
niuniuanran <irene.anran.niu@gmail.com>
2020-09-20 22:09:07 +1200

    Configure AWS Amplify before rendering App

--- src/index.js
+++ src/index.js
@@ -4,6 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Amplify } from 'aws-amplify';
+import config from './config';
+
+Amplify.configure({
+    // Amplify refers to Cognito as Auth, S3 as Storage, and API Gateway as API.
+    Auth: {
+        // The mandatorySignIn flag for Auth is set to true because we want our users to be signed in before they can interact with our app.
+        mandatorySignIn: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    },
+    Storage: {
+        region: config.s3.REGION,
+        bucket: config.s3.BUCKET,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID
+    },
+    API: {
+        endpoints: [
+            {
+                name: "notes",
+                endpoint: config.apiGateway.URL,
+                region: config.apiGateway.REGION
+            },
+        ]
+    }
+});
 
 ReactDOM.render(
         <Router>
