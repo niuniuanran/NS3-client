00dffa24e147f02c3d793a9f2e050b93233b9fd9
niuniuanran <irene.anran.niu@gmail.com>
2020-09-20 21:40:49 +1200

    Change homepage to show customised content

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,8 +5,10 @@ export default function Home() {
     return (
         <div className="Home">
             <div className="lander">
-                <h1>Scratch</h1>
-                <p>A simple note taking app</p>
+                <h1>NS3</h1>
+                <p>Note-taking app based on Scratch by Serverless Stack</p>
+                <p>Anran's first web application on AWS serverless infrastructure</p>
+
             </div>
         </div>
     );
