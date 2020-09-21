5bddaf77f79227a837b4ca3bd5520b178dfbd517
niuniuanran <irene.anran.niu@gmail.com>
2020-09-21 22:11:36 +1200

    Add querystring function to extract the query string from url

--- src/components/UnauthenticatedRoute.js
+++ src/components/UnauthenticatedRoute.js
@@ -2,6 +2,22 @@ import React from "react";
 import {Route, Redirect, useLocation} from "react-router-dom";
 import {useAppContext} from "../libs/contextLib";
 
+function querystring(name, url = window.location.href) {
+    name = name.replace(/[[]]/g, "\\$&");
+
+    const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i");
+    const results = regex.exec(url);
+
+    if (!results) {
+        return null;
+    }
+    if (!results[2]) {
+        return "";
+    }
+
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 export default function AuthenticatedRoute({children, ...rest}) {
 
     // useAppContext is the consumer of the AppContext. It returns the values in the context provider.
